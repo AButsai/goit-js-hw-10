321eae176f8ebb524863e9e0932d4cd0238849e4
AButsai <butsai@ukr.net>
2022-02-05 10:53:16 +0200

    firs commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
+import fetchCountries from './js/fetchCountries.js';
+import { debounce } from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+Notify.info('Too many matches found. Please enter a more specific name.');
+Notify.failure('Oops, there is no country with that name');
