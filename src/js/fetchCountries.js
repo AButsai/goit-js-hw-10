2fd44b87362e9c75e172aab779bb55c540a8c4d1
AButsai <butsai@ukr.net>
2022-02-07 02:57:16 +0200

    http-request

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,5 @@
-export const fetchCountries = name => {};
+export const fetchCountries = name => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?sort=name&fields=name,name.official,capital,population,flags,languages`,
+  ).then(response => response.json());
+};
